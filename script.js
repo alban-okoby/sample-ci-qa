c47f6aa42ca8b009e1fa5cd98639dde12c97b9b5
alban-okoby <albanokoby225@gmail.com>
2026-06-26 14:50:15 +0000

    Initial commit: basic sample of todo app

---
+++ script.js
@@ -0,0 +1,18 @@
+const tasks = [];
+
+function render() {
+  const ul = document.getElementById('task-list');
+  ul.innerHTML = '';
+  tasks.forEach((task, idx) => {
+    const li = document.createElement('li');
+    li.textContent = task.name;
+    ul.appendChild(li);
+  });
+}
+
+document.getElementById('add-btn').onclick = () => {
+  const input = document.getElementById('new-task');
+  tasks.push({ id: Date.now(), name: input.value });
+  input.value = '';
+  render();
+};
